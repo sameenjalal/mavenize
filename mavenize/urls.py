b5b579be1b3cd890966ad9301d076e7e967f1117
Dennis Ai <dennis.q.ai@gmail.com>
2012-02-01 19:29:15 -0600

    Imported bootstrap, basic templates, and set up basic urls

--- mavenize/urls.py
+++ mavenize/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.static import *
+from django.conf import settings
+
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mavenize.views.home', name='home'),
-    # url(r'^mavenize/', include('mavenize.foo.urls')),
+	url(r'^$', direct_to_template, {'template': 'index.html'}, name='index'),
+	url(r'', include('social_auth.urls')),
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+		{'document_root': settings.MEDIA_ROOT}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+     url(r'^admin/', include(admin.site.urls)),
 )
