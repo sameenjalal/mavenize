a34268638abc76ed1e0eba88471dd3c343adecdc
Sameen Jalal <sameenjalal@gmail.com>
2012-02-11 18:45:52 -0500

    asdf

--- mavenize/social_graph/models.py
+++ mavenize/social_graph/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Following(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.IntegerField()
-    follow = models.IntegerField()
+    fb_user = models.IntegerField(default=0)
+    follow = models.IntegerField(default=0)
 
 class Follower(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.IntegerField()
-    follow = models.IntegerField()
+    fb_user = models.IntegerField(default=0)
+    follow = models.IntegerField(default=0)
