085c3d08b085a020465b8737dd8bdf9034b22524
Sameen Jalal <sameenjalal@gmail.com>
2012-02-12 17:26:24 -0500

    Fixed urls to search for

--- scripts/parse_genres/parse_genres.py
+++ scripts/parse_genres/parse_genres.py
@@ -1,10 +1,10 @@
 import json
 from pprint import pprint
 
-json_data=open('Genres.json')
+json_data=open('json_genres.json')
 data = json.load(json_data)
 
 for x in data['d']:
-    print x['__metadata']['uri']
+    print x['__metadata']['uri'] + "/Titles?$format=json"
 
 json_data.close()
