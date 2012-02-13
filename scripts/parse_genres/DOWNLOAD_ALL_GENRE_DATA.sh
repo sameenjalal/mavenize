68bdc5a4face0aa096a9f3d3dbfc9a9fca2662ba
Sameen Jalal <sameenjalal@gmail.com>
2012-02-13 11:41:58 -0800

    Finished downloading all genre data

---
+++ scripts/parse_genres/DOWNLOAD_ALL_GENRE_DATA.sh
@@ -0,0 +1,22 @@
+FILE="genre_urls.txt"
+node=($(cat $FILE))
+NODES=${node[@]}
+
+#for i in $NODES; do
+	#echo "\n\n$i\n\n"
+	#echo "wget $i -O `awk -F'[(|)]' '{print $2}' genre_urls.txt`"
+#done
+
+c=0
+for i in $NODES ; do
+	l1[$c]=$i
+	c=$(($c+1))
+done
+
+c=0
+for j in `awk -F'[(|)]' '{print $2}' genre_urls.txt` ; do
+	#echo ${l1[$c]} `echo $j | sed 's/%//g' | sed 's/20/_/g' | sed 's/26/_/g' | sed 's/2C/_/g' | sed 's/___/_/g' | sed 's/_th/20th/g' | sed 's/C3A9/e/g'`
+	wget ${l1[$c]} -O `echo $j | sed 's/%//g' | sed 's/20/_/g' | sed 's/26/_/g' | sed 's/2C/_/g' | sed 's/___/_/g' | sed 's/_th/20th/g' | sed 's/C3A9/e/g'`
+	#echo ${l1[$c]} $j
+	c=$(($c+1))
+done
--- scripts/parse_genres/parse_genres.py
+++ scripts/parse_genres/parse_genres.py
@@ -1,10 +1,8 @@
 import json
-from pprint import pprint
-
 json_data=open('json_genres.json')
 data = json.load(json_data)
 
 for x in data['d']:
-    print x['__metadata']['uri'] + "/Titles?$format=json"
+    print x['Name'] + "|" + x['__metadata']['uri'] + "/Titles?$format=json"
 
 json_data.close()
