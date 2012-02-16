8bbb25e61a5a5bd003699c6b099b01c4c7b39f30
Sameen Jalal <sameenjalal@gmail.com>
2012-02-16 12:32:58 -0800

    finished analyzing files and correcting corrupted data

--- scripts/parse_titles/PARSE_TITLES.py
+++ scripts/parse_titles/PARSE_TITLES.py
@@ -1,54 +1,76 @@
 import json
-json_data=open('titles/00_titles.json')
-data = json.load(json_data)
 
-print data['catalog_titles']['catalog_title'][0].keys()
+for file_num in range( 0, 58 ):
+    json_file_name = "titles/" + str(file_num) + "_titles.json"
+    json_data=open( json_file_name )
+    data = json.load(json_data)
 
-try:
-    for item in range(0,len(data['catalog_titles']['catalog_title'])):
-        print "Title:"
-        print data['catalog_titles']['catalog_title'][item]['title']['regular']
-        print
+#print data['catalog_titles']['catalog_title'][0].keys()
 
-        print "Netflix ID:"
-        print data['catalog_titles']['catalog_title'][item]['id']
-        print
+    try:
+        for item in range(0,len(data['catalog_titles']['catalog_title'])):
 
-        print "Genres:"
-        for x in data['catalog_titles']['catalog_title'][item]['category']:
-            print x['term']
-        print
+            print "Title:"
+            print data['catalog_titles']['catalog_title'][item]['title']['regular']
+            print
 
-        print "Release Year:"
-        print data['catalog_titles']['catalog_title'][item]['release_year']
-        print
+            print "Netflix ID:"
+            print data['catalog_titles']['catalog_title'][item]['id']
+            print
 
-        print "Image URL"
-        print data['catalog_titles']['catalog_title'][item]['box_art']['large']
-        print
+            print "Genres:"
+            for x in data['catalog_titles']['catalog_title'][item]['category']:
+                try:
+                    if x:
+                        print x['term']
+                except:
+                    print x
+                    pass
+            print
 
-        print "Average Rating"
-        print data['catalog_titles']['catalog_title'][0]['average_rating']
-        print
+            print "Release Year:"
+            print data['catalog_titles']['catalog_title'][item]['release_year']
+            print
 
-        for x in data['catalog_titles']['catalog_title'][item]['link']:
-            for key,val in x.iteritems():
-                if key == "synopsis":
-                    print "Synopsis"
-                    print val
-                    print
-                if key == "awards":
-                    print "Awards:"
-                    print val
-                    print
-                if key == "catalog_titles":
-                    print "Similars"
-                    for a in val['link']:
-                        print a
-                    print
-except:
-    print "fucked up"
-    print item
-    raise
+            print "Image URL"
+            print data['catalog_titles']['catalog_title'][item]['box_art']['large']
+            print
 
-json_data.close()
+            print "Average Rating"
+            print data['catalog_titles']['catalog_title'][0]['average_rating']
+            print
+
+            for x in data['catalog_titles']['catalog_title'][item]['link']:
+                for key,val in x.iteritems():
+                    if key == "synopsis":
+                        print "Synopsis"
+                        print val
+                        print
+                    if key == "awards":
+                        print "Awards:"
+                        print val
+                        print
+                    if key == "catalog_titles":
+                        print "Similars"
+                        try:
+                            a = val['link']
+                        except:
+                            print val
+                            continue
+                        for b in range( 0 , len( a )):
+                            try:
+                                print a[b]['title']
+                            except:
+                                try:
+                                    for k,v in val['link'].iteritems():
+                                        if k == "title":
+                                            print v
+                                except:
+                                    print val
+                                    pass
+    except:
+        print "fucked up"
+        print item
+        raise
+
+    json_data.close()
