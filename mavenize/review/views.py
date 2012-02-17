919561330b5ed2203e61b187ff0f7bcfb8ede9e2
Dennis Ai <dennis.q.ai@gmail.com>
2012-02-16 22:39:17 -0600

    Updated user profile view and thank view

--- mavenize/review/views.py
+++ mavenize/review/views.py
@@ -5,6 +5,9 @@ from django.http import Http404
 from mavenize.review.models import Review
 from mavenize.review.models import Thanks
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def thank(request, review_id):
     if request.is_ajax():
         review = get_object_or_404(Review, pk=review_id)
@@ -13,8 +16,16 @@ def thank(request, review_id):
             review = review
         )
         if created:
+            receiver_profile = review.user.get_profile()
+            giver_profile = request.user.get_profile()
+
             review.up_votes += 1
             review.save()
+            receiver_profile.thanks_received += 1
+            receiver_profile.save()
+            giver_profile.thanks_given += 1
+            giver_profile.save()
+
         return redirect(request.META.get('HTTP_REFERER', None))
     
     else:
--- mavenize/user_profile/views.py
+++ mavenize/user_profile/views.py
@@ -4,18 +4,27 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from mavenize.movie.models import Movie
 from mavenize.review.models import Review
+from mavenize.review.models import Thanks
 from mavenize.social_graph.models import Following
 from mavenize.social_graph.models import Follower
 
 from django.contrib.auth.decorators import login_required
 
+from itertools import chain
+
 @login_required
 def profile(request, id):
     target_user = User.objects.get(pk=id) 
+    target_profile = target_user.get_profile()
+    positive_movies = []
+    neutral_movies = []
+    negative_movies = []
+
     following = Following.objects.filter(fb_user=id).values_list(
         'follow', flat=True)[:3]
     followers = Follower.objects.filter(fb_user=id).values_list(
         'follow', flat=True)[:3]
+
     positive_reviews = Review.objects.filter(
         user=id, rating=2).values_list('table_id_in_table', flat=True)
     neutral_reviews = Review.objects.filter(
@@ -28,6 +37,8 @@ def profile(request, id):
         'image', 'url')
     negative_movies = Movie.objects.filter(pk__in=negative_reviews).values(
         'image', 'url')
+    
+    # Filter into positive, neutral, and negative reviews
 
     return render_to_response('user_profile.html', {
            'target_user': target_user,
@@ -35,6 +46,9 @@ def profile(request, id):
            'followers': followers,
            'positive_movies': positive_movies,
            'neutral_movies': neutral_movies,
-           'negative_movies': negative_movies
+           'negative_movies': negative_movies,
+           'review_count': target_profile.reviews,
+           'thanks_received': target_profile.thanks_received,
+           'thanks_given': target_profile.thanks_given
         },
         RequestContext(request)) 
