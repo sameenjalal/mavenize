1fd2b65d7dfbb0b5755aca33812a09eb475c3693
Dennis Ai <dennis.q.ai@gmail.com>
2012-02-18 14:05:38 -0600

    Added load more button to movie profile

--- mavenize/movie/views.py
+++ mavenize/movie/views.py
@@ -34,28 +34,39 @@ def genre(request, genre):
 
 @login_required
 def profile(request, title):
-    movie = get_object_or_404(Movie, url=title)
-    reviews = aggregate_reviews(
-        request.user.id,
-        Review.objects.filter(table_id_in_table=movie.movie_id).values(
-            'review_id',
-            'user',
-            'text',
-            'up_votes',
-            'created_at'
-        )[:10])
-
-    rating = 0
-    if reviews:
-        rating = int(Review.objects.filter(
-            table_id_in_table=movie.movie_id).aggregate(
+    context = load_reviews(request, title, 1)
+    context['rating'] = 0
+    if context['reviews']:
+        context['rating'] = int(Review.objects.filter(
+            table_id_in_table=context['movie'].movie_id).aggregate(
                 Avg('rating'))['rating__avg']*50)
+    context['form'] = ReviewForm()
     
-    return render_to_response('movie_profile.html', {
-        'movie': movie,
-        'rating': rating,
-        'reviews': reviews,
-        'form': ReviewForm(),
-        },
-        context_instance=RequestContext(request))
+    return render_to_response('movie_profile.html', 
+        context,
+        context_instance=RequestContext(request)
+    )
 
+def load_reviews(request, title, page):
+    context = {}
+    
+    context['movie'] = get_object_or_404(Movie, url=title)
+    context['reviews'] = aggregate_reviews(
+        request.user.id,
+        Review.objects.filter(
+            table_id_in_table=context['movie'].movie_id).values(
+                'review_id',
+                'user',
+                'text',
+                'up_votes',
+                'created_at'
+            )[10*(int(page)-1):10*int(page)])
+
+    if request.is_ajax():
+        return render_to_response(
+            'partials/movie_review.html',
+            context,
+            context_instance=RequestContext(request)
+        )
+
+    return context
