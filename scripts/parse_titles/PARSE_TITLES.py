0c56f32ebf414c89f4cf25eb53f03feab190816f
Sameen Jalal <sameenjalal@gmail.com>
2012-02-20 20:25:31 -0800

    asdf

--- scripts/parse_titles/PARSE_TITLES.py
+++ scripts/parse_titles/PARSE_TITLES.py
@@ -5,53 +5,72 @@ for file_num in range( 0, 58 ):
     json_data=open( json_file_name )
     data = json.load(json_data)
 
-#print data['catalog_titles']['catalog_title'][0].keys()
+    #print data['catalog_titles']['catalog_title'][0].keys()
 
     try:
         for item in range(0,len(data['catalog_titles']['catalog_title'])):
+            fields = []
+            values = []
 
-            print "Title:"
-            print data['catalog_titles']['catalog_title'][item]['title']['regular']
-            print
+            #print "Title: format text"
+            title = data['catalog_titles']['catalog_title'][item]['title']['regular']
+            #print title
+            fields.append( "title" )
+            values.append( title )
+            #print
 
-            print "Netflix ID:"
-            print data['catalog_titles']['catalog_title'][item]['id']
-            print
+            #print "Netflix ID: format text"
+            #print data['catalog_titles']['catalog_title'][item]['id']
+            #print
 
-            print "Genres:"
+            #print "Genres: format list"
+            all_genres = []
             for x in data['catalog_titles']['catalog_title'][item]['category']:
                 try:
                     if x:
-                        print x['term']
+                        all_genres.append( x['term'] )
+                        #print x['term']
                 except:
-                    print x
+                    all_genres.append( x )
+                    #print x
                     pass
-            print
+            #print all_genres
+            if all_genres:
+                fields.append( "genre" )
+                values.append( all_genres )
+            #print
 
-            print "Release Year:"
-            print data['catalog_titles']['catalog_title'][item]['release_year']
-            print
+            #print "Release Year: format text"
+            release_date = data['catalog_titles']['catalog_title'][item]['release_year']
+            fields.append( "release_date" )
+            values.append( release_date )
+            #print
 
-            print "Image URL"
-            print data['catalog_titles']['catalog_title'][item]['box_art']['large']
-            print
+            #print "Image URL: format text"
+            pic = data['catalog_titles']['catalog_title'][item]['box_art']['large']
+            # TODO: download pics and store it in db
+            #print
 
-            print "Average Rating"
-            print data['catalog_titles']['catalog_title'][0]['average_rating']
-            print
+            #print "Average Rating: format number"
+            #print data['catalog_titles']['catalog_title'][0]['average_rating']
+            #print
 
             for x in data['catalog_titles']['catalog_title'][item]['link']:
                 for key,val in x.iteritems():
                     if key == "synopsis":
-                        print "Synopsis"
-                        print val
-                        print
+                        #print "Synopsis: format text"
+                        #print val
+                        fields.append( "synopsis" )
+                        values.append( val )
+                        #print
                     if key == "awards":
-                        print "Awards:"
-                        print val
-                        print
+                        #print "Awards: format json list"
+                        fields.append( "awards" )
+                        values.append( val )
+                        #print val
+                        #print
                     if key == "catalog_titles":
-                        print "Similars"
+                        #print "Similars: format list"
                         all_similars = []
                         try:
                             a = val['link']
@@ -72,7 +91,32 @@ for file_num in range( 0, 58 ):
                                     #print val
                                     all_similars.append( val )
                                     pass
-                        print all_similars
+                        #print all_similars
+                        if all_similars:
+                            fields.append( "similars" )
+                            values.append( all_similars )
+            # Starting to generate insert statemetns for fields and values
+            table_name = "mavenize_development"
+            values_stmt = "'"
+            for fin in range( 0, len( fields ) ):
+                item = values[ fin ]
+                if isinstance( item , basestring ):
+                    values_stmt += item.replace( "'", "''" ) + "','"
+                else:
+                    item = json.dumps( item ).replace( "'", "''")
+                    item = item.replace( "\"", "\\\"" )
+                    values_stmt += item + "','"
+            values_stmt = values_stmt.rstrip( "'" )
+            values_stmt = values_stmt.strip( "," )
+
+            stmt = 'INSERT INTO %s (%s) VALUES (%s);' % (
+                    table_name,
+                    ','.join(fields),
+                    values_stmt
+                    )
+
+            print stmt
+            #print values
     except:
         print "fucked up"
         print item
