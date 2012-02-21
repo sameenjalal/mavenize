04bf80ae7e16e42ef62192651f7605cb1791af26
Sameen Jalal <sameenjalal@gmail.com>
2012-02-21 16:53:22 -0500

    asdf'

--- scripts/tmdb/test.py
+++ scripts/tmdb/test.py
@@ -3,14 +3,24 @@ import pprint
 
 
 tmdb.configure( "c3648d284b99debdb865cf318248b209" )
-movie = tmdb.Movie("Living Legends of Comedy: Real Talk")
-credits = tmdb.Credits("Living Legends of Comedy: Real Talk")
+o = 0
+try:
+    for line in open('all_titles_from_psql.txt','r'):
+        line = line.strip()
+        movie = tmdb.Movie( line )
 
-movie_id = movie.get_id()
+        movie_id = movie.get_id()
+        title_poster = {"title": line, "poster": movie.get_poster()}
+        o += 1
+        if o > 5:
+            break
+except: pass
+print title_poster
 
+
+"""
 print movie.full_info( movie_id )
 print movie.get_total_results()
-"""
 print movie.is_adult()
 print movie.get_genres( movie_id )
 print movie.get_collection_name( movie_id )
