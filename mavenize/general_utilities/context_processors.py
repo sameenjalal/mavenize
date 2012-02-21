99d4ab044c06a970969e4794facde9da2a869bb9
Dennis Ai <dennis.q.ai@gmail.com>
2012-02-20 20:07:44 -0600

    Added notification button to menubar

--- mavenize/general_utilities/context_processors.py
+++ mavenize/general_utilities/context_processors.py
@@ -2,3 +2,6 @@ from haystack.forms import SearchForm
 
 def search(request):
     return {'search': SearchForm()}
+
+def thanks(request):
+    return {'thanks': request.user.get_profile().thanks_received }
