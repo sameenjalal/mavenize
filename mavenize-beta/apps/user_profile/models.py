695419581bb775fb583869886ac3fececa4bce16
Dennis Ai <dennis.q.ai@gmail.com>
2012-04-14 17:29:34 -0500

    Lengthened timeout to 30 seconds

--- mavenize-beta/apps/user_profile/models.py
+++ mavenize-beta/apps/user_profile/models.py
@@ -77,8 +77,8 @@ def update_user_profile(sender, user, response, details, **kwargs):
         try:
             url = ("http://graph.facebook.com/%s/picture" %         
                 response["id"])
-            avatar = urlopen(url+'?type=large', timeout=15).read()
-            thumbnail = urlopen(url, timeout=15).read()
+            avatar = urlopen(url+'?type=large', timeout=30).read()
+            thumbnail = urlopen(url, timeout=30).read()
             if not created:
                 if (hashlib.sha1(profile.thumbnail.read()).digest()
                         != hashlib.sha1(thumbnail).digest()):
